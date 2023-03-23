91c579a7d2be584b34de42b4c587959308a845d2
MarianBaba <marianbaba16@gmail.com>
2023-03-23 21:15:13 +0100

    + validation func: Communication principal is duplicate -> generate warning

--- packages/core/src/validator.ts
+++ packages/core/src/validator.ts
@@ -1,5 +1,5 @@
 import type { MaybePromise, ValidationAcceptor, ValidationChecks } from "langium"
-import type { KantAstType, Protocol, AuthenticationCheck } from "./generated/ast"
+import type { KantAstType, Protocol, AuthenticationCheck, Communication } from "./generated/ast"
 import { isFunctionDef } from "./generated/ast"
 import type { KantServices } from "./module"
 
@@ -11,7 +11,8 @@ export function registerValidationChecks(services: KantServices): void {
     const validator = services.validation.KantValidator
     const checks: ValidationChecks<KantAstType> = {
         Protocol: [KantValidator.checkUniqueFunctionName],
-        AuthenticationCheck: [KantValidator.checkAuthenticationPrincipalIsNotDuplicate]
+        AuthenticationCheck: [KantValidator.checkAuthenticationPrincipalIsNotDuplicate],
+        Communication: [KantValidator.checkCommunicationPrincipalIsNotDuplicate]
     }
     registry.register(checks, validator)
 }
@@ -22,14 +23,9 @@ export function registerValidationChecks(services: KantServices): void {
 export const KantValidator = {
     /**
      * 
-     * @param authenticationCheck 
+     * @param protocol 
      * @param accept 
      */
-    checkAuthenticationPrincipalIsNotDuplicate: (authenticationCheck: AuthenticationCheck, accept: ValidationAcceptor): MaybePromise<void> => {
-        if (authenticationCheck.principal === authenticationCheck.target) {
-            accept(`warning`, `Authentication check is redundant`, { node: authenticationCheck })
-        }
-    },
     checkUniqueFunctionName: (protocol: Protocol, accept: ValidationAcceptor): MaybePromise<void> => {
         const functionDefs = protocol.elements.filter(isFunctionDef)
         const alreadyDefinedNames = new Set<string>()
@@ -40,6 +36,28 @@ export const KantValidator = {
             }
             alreadyDefinedNames.add(fd.name)
         }
+    },
+    /**
+     * 
+     * @param authenticationCheck 
+     * @param accept 
+     */
+    checkAuthenticationPrincipalIsNotDuplicate: (authenticationCheck: AuthenticationCheck, accept: ValidationAcceptor): MaybePromise<void> => {
+        if (authenticationCheck.principal === authenticationCheck.target) {
+            accept(`warning`, `Authentication check is redundant`, { node: authenticationCheck })
+        }
+    },
+    /**
+     * 
+     * @param communication 
+     * @param accept 
+     */
+    checkCommunicationPrincipalIsNotDuplicate: (communication: Communication, accept: ValidationAcceptor): MaybePromise<void> => {
+        communication.from.forEach(principal => {
+            if (communication.to.includes(principal)) {
+                accept(`warning`, `Communication is redundant: "${principal}" already knows this knowledge`, {node: communication})
+            }
+        })
     }
 }
 export type KantValidator = typeof KantValidator
