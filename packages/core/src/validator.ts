e5a7809874a1a7007a4e6f948eeab2403f1427a9
Marian Baba <marianbaba16@gmail.com>
2023-04-25 16:23:18 +0200

    + validation func: check knowledge name is duplicate in same scope -> generate error

--- packages/core/src/validator.ts
+++ packages/core/src/validator.ts
@@ -17,7 +17,8 @@ export function registerValidationChecks(services: KantServices): void {
             KantValidator.checkKnowledgeUsage,
             KantValidator.checkUnknownFunctionUsage,
             KantValidator.checkCommunicationAttemptOfKnowledgeUnknownToSender,
-            KantValidator.checkInversionFunctionIsCorrect
+            KantValidator.checkInversionFunctionIsCorrect,
+            KantValidator.checkKnowledgeIsNotDuplicateInSameScope
         ],
         AuthenticationCheck: [
             KantValidator.checkAuthenticationPrincipalIsNotDuplicate
@@ -647,6 +648,78 @@ export const KantValidator = {
                         accept(`error`, `Inversion function ${inverter.ref.name} has incompatible number of parameters compared to inverted function`, { node: propertyDef })
 
                     }
+                }
+            }
+        })
+    },
+    /**
+     * 
+     * @param protocol 
+     * @param accept 
+     */
+    checkKnowledgeIsNotDuplicateInSameScope: (protocol: Protocol, accept: ValidationAcceptor): MaybePromise<void> => {
+        const definedBuiltinKnowledge = new Set<string>()
+        protocol.elements.filter(isKnowledgeBase).forEach(knowledgeBase => {
+            if (isKnowledgeDef(knowledgeBase.knowledge)) {
+                if (isKnowledgeDefBuiltin(knowledgeBase.knowledge)) {
+                    knowledgeBase.knowledge.name.forEach(knowledgeName => {
+                        if (definedBuiltinKnowledge.has(knowledgeName)) {
+                            accept(`error`, `Duplicate knowledge definition`, { node: knowledgeBase.knowledge })
+                        } else {
+                            definedBuiltinKnowledge.add(knowledgeName)
+                        }
+                    })
+                }
+                if (isKnowledgeDefCustom(knowledgeBase.knowledge)) {
+                    if (isKnowledgeSet(knowledgeBase.knowledge.value)) {
+                        const setKnowledge = new Set<string>()
+                        knowledgeBase.knowledge.value.content.forEach(setValue => {
+                            if (isKnowledgeDefBuiltin(setValue)) {
+                                setValue.name.forEach(n => {
+                                    if (setKnowledge.has(n)) {
+                                        accept(`error`, `Duplicate knowledge definition`, { node: knowledgeBase.knowledge })
+                                    } else {
+                                        setKnowledge.add(n)
+                                    }
+                                })
+                            }
+                            if (isKnowledgeRef(setValue)) {
+                                if (setKnowledge.has(setValue.ref.concat(setValue.access.toString()))) {
+                                    accept(`error`, `Duplicate knowledge definition`, { node: knowledgeBase.knowledge })
+                                } else {
+                                    setKnowledge.add(setValue.ref.concat(setValue.access.toString()))
+                                }
+                            }
+                        })
+                    }
+                }
+            }
+
+            if (isKnowledgeValue(knowledgeBase.knowledge)) {
+                if (isKnowledgeSet(knowledgeBase.knowledge)) {
+                    knowledgeBase.knowledge.content.forEach(setContent => {
+                        if (isKnowledgeDefBuiltin(setContent)) {
+                            setContent.name.forEach(name => {
+                                if (definedBuiltinKnowledge.has(name)) {
+                                    accept(`error`, `Duplicate knowledge definition`, { node: knowledgeBase.knowledge })
+                                } else {
+                                    definedBuiltinKnowledge.add(name)
+                                }
+                            })   
+                        }
+                    })
+                }
+                if (isKnowledgeList(knowledgeBase.knowledge)) {
+                    const listKnowledge = new Set<string>()
+                    knowledgeBase.knowledge.values.forEach(listVal => {
+                        if (ast.isKnowledgeRef(listVal)) {
+                            if (listKnowledge.has(listVal.ref.concat(listVal.access.toString()))) {
+                                accept(`error`, `Duplicate knowledge definition`, { node: knowledgeBase.knowledge })
+                            } else {
+                                listKnowledge.add(listVal.ref.concat(listVal.access.toString()))
+                            }
+                        }
+                    })
                 }
             }
         })
